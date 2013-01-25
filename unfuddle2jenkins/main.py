ddba801111cf33283917a3b5a659519e03942d2e
Jakub Jaroszewski <jakub.jaroszewski@gmail.com>
2013-01-25 20:19:17 +0000

    Format

--- unfuddle2jenkins/main.py
+++ unfuddle2jenkins/main.py
@@ -10,9 +10,9 @@ unfuddle2jenkins_map = None
 
 
 def get_jenkins_url_for_repository_id(repository_id):
-    '''
+    """
     Retrieves Jenkins trigger url for given repository id.
-    '''
+    """
     global unfuddle2jenkins_map
     if unfuddle2jenkins_map is None:
         unfuddle2jenkins_map = eval(os.environ[MAP_ENV_VAR_NAME])
@@ -20,18 +20,18 @@ def get_jenkins_url_for_repository_id(repository_id):
 
 
 def get_repository_id_from_xml(changeset_xml):
-    '''
+    """
     Super simple. Gets repository_id from changeset XML.
-    '''
+    """
     # I hate XML
     dom = minidom.parseString(changeset_xml)
     return int(dom.getElementsByTagName('repository-id')[0].firstChild.data)
 
 
 def application(env, start_response):
-    '''
+    """
     WSGI application.
-    '''
+    """
     start_response('200 OK', [('Content-Type', 'text/plain')])
     try:
         posted_data = env['wsgi.input'].read(1024)
@@ -45,4 +45,4 @@ def application(env, start_response):
             log.warning('No Jenkins url for repository id %d' % repository_id)
     except Exception:
         log.exception('Something went wrong')
-    return "done"
+    return 'done'
